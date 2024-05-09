f254cbbbcf4cb22308b6c856f68dde1f6c1b9be9
AlvaroGalvezBarba <122776088+AlvaroGalvezBarba@users.noreply.github.com>
2024-05-09 22:14:00 +0200

    equipos

--- src/app/views/equipos/equipos.component.ts
+++ src/app/views/equipos/equipos.component.ts
@@ -8,8 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './equipos.component.css'
 })
 export class EquiposComponent {
-  public teams:{coche:string, logo:string, nombreLogo:string, posicion:string, puntos:string, piloto:string, nombrePiloto:string, nombrePiloto1:string, piloto1:string}[] = [
-    {coche : "Red_Bull", logo:"redBull", nombreLogo: "Red Bull", posicion:"1", puntos:"239 PTS", piloto:"S.P", nombrePiloto:"Perez", nombrePiloto1:"Verstappen", piloto1:"M.V"},
-    {coche : "Ferrari", logo:"Ferrari", nombreLogo: "Ferrari", posicion:"2", puntos:"187 PTS", piloto:"C.L", nombrePiloto:"Leclerc", nombrePiloto1:"Sainz", piloto1:"C.S"},
-  ]
+  public teams:{equipo:string, puntos:string, piloto:string, nombrePiloto:string, nombrePiloto1:string, piloto1:string}[] = [
+    {equipo : "Red Bull", puntos:"239 PTS", piloto:"S.P", nombrePiloto:"Perez", nombrePiloto1:"Verstappen", piloto1:"M.V"},
+    {equipo : "Ferrari", puntos:"187 PTS", piloto:"C.L", nombrePiloto:"Leclerc", nombrePiloto1:"Sainz", piloto1:"C.S"},
+    {equipo : "McLaren", puntos:"124 PTS", piloto:"L.N", nombrePiloto:"Norris", nombrePiloto1:"Piastri", piloto1:"O.P"},
+    {equipo : "Mercedes", puntos:"64 PTS", piloto:"L.H", nombrePiloto:"Hamilton", nombrePiloto1:"Russell", piloto1:"G.R"},
+    {equipo : "Aston Martin", puntos:"42 PTS", piloto:"Padre", nombrePiloto:"Alonso", nombrePiloto1:"Stroll", piloto1:"L.S"},
+    {equipo : "RB", puntos:"19 PTS", piloto:"Y.T", nombrePiloto:"Tsunoda", nombrePiloto1:"Ricciardo", piloto1:"D.R"},
+    {equipo : "Haas", puntos:"7 PTS", piloto:"N.H", nombrePiloto:"Hulkenberg", nombrePiloto1:"Magnussen", piloto1:"K.M"},
+    {equipo : "Alpine", puntos:"1 PTS", piloto:"E.C", nombrePiloto:"Ocon", nombrePiloto1:"Gasly", piloto1:"P.G"},
+    {equipo : "Williams", puntos:"0 PTS", piloto:"A.A", nombrePiloto:"Albon", nombrePiloto1:"Sargeant", piloto1:"Logan.S"},
+    {equipo : "Kick Sauber", puntos:"0 PTS", piloto:"V.B", nombrePiloto:"Bottas", nombrePiloto1:"Zhou", piloto1:"Z.G"},
+    ]
 }
